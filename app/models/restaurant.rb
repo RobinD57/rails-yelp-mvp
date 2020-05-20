2b4a7935c417b50c693fa5d3eab101a6397a71f3
RobinD57 <robin.davidseth@gmail.com>
2020-05-20 11:51:58 +0200

    finished restaurant and review models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = %w(chinese italian japanese french belgian)
+
+  has_many :reviews, dependent: :destroy
+  validates :address, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
